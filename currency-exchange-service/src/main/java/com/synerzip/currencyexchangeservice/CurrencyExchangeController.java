f84c30119c0bd4dce868354f54a6558c5ad8ca84
Vivek Tiwari <vivekt@synerzipune.local>
2018-04-27 12:21:34 +0530

    another microservice - currency exchange service

---
+++ currency-exchange-service/src/main/java/com/synerzip/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,28 @@
+package com.synerzip.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private ExchangeValueRepository repository;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue exchangeValue(@PathVariable String from, @PathVariable String to) {
+        ExchangeValue exchangeValue =repository.findByFromAndTo(from,to);
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        return exchangeValue;
+    }
+
+}
+
