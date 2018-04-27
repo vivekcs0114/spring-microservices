5a2da615d6799466fc359ee6e96f3885756a775a
Vivek Tiwari <vivekt@synerzipune.local>
2018-04-27 19:34:31 +0530

    new microservice accessing resource from another microservice

---
+++ currency-conversion-service/src/main/java/com/synerzip/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,30 @@
+package com.synerzip.currencyconversionservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    private CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to
+            , @PathVariable BigDecimal quantity) {
+        Map<String, String> urlVariables = new HashMap();
+        urlVariables.put("from", from);
+        urlVariables.put("to", to);
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8001/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversionBean.class,
+                urlVariables);
+        CurrencyConversionBean response = responseEntity.getBody();
+        return new CurrencyConversionBean(response.getId(),from,to,response.getConversionMultiple(),
+               quantity,quantity.multiply(response.getConversionMultiple()),response.getPort());
+    }
+}
