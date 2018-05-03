8232e0912336e62079e56ed9079b012db4733a72
Vivek Tiwari <vivekt@synerzipune.local>
2018-05-03 11:33:06 +0530

    ribbon impl for load balancing

--- currency-conversion-service/src/main/java/com/synerzip/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/synerzip/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,10 +1,13 @@
 package com.synerzip.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-conversion-service", url = "localhost:8001")
+//@FeignClient(name = "currency-conversion-service", url = "localhost:8001")
+@FeignClient(name = "currency-conversion-service")
+@RibbonClient("currency-conversion-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
